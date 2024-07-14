78a475f4e67d1cd516e5cf933e506203ef16296b
palkar22 <121425853+palkar22@users.noreply.github.com>
2024-07-14 14:15:51 +0530

    Update intel_sentiment_analyzer.py

--- intel_sentiment_analyzer.py
+++ intel_sentiment_analyzer.py
@@ -103,6 +103,8 @@ if not st.session_state.get("chat_mode", False):
     # Main page content
     st.title("Intel Product Sentimental Analysis")
     st.markdown("<div style='text-align: right; font-size: 20px;'>By N Palani Karthik</div></br></br>", unsafe_allow_html=True)
+    st.markdown("### MODEL WORK FLOW ")
+    st.image("WORKING.png", use_column_width=True) 
     st.markdown("### Sentiment Distribution used to train model")
     st.image("download.png", use_column_width=True) 
     st.markdown("### Model Evaluation Metirics\n")
